aded5c2813b4d9b5a092000a2c5fd531a5c49f82
nic3guy <krabik2342@gmail.com>
2022-07-02 12:09:40 +0300

    57. Refactoring Our Routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const toursDB = `${__dirname}/dev-data/data/tours-simple.json`;
 const tours = JSON.parse(fs.readFileSync(toursDB));
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -16,9 +16,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   const { id: strId } = req.params;
   const id = +strId;
 
@@ -35,9 +35,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     tour,
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = {
     id: newId,
@@ -50,9 +50,9 @@ app.post('/api/v1/tours', (req, res) => {
       addedTour: newTour,
     });
   });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const { id: strId } = req.params;
   const id = +strId;
 
@@ -69,9 +69,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       updatedTour: '<Updated tour here...>',
     },
   });
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   const { id: strId } = req.params;
   const id = +strId;
 
@@ -86,7 +86,14 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 8000;
 app.listen(port, () => {
