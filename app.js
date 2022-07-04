a6239452f467baa59195dd2860d9b7a646bd5234
Lonerr7 <krabik2342@gmail.com>
2022-07-04 10:11:06 +0300

    67. Environment Variables

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // ========== Middlewares ==================
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+app.use(express.static(`${__dirname}/public`));
 
 // ========== Mounting Routers ==================
 app.use('/api/v1/tours', tourRouter);
