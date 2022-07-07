74faaf4bdb73d7a7e81ebb42de55ee674e4332b9
nic3guy <krabik2342@gmail.com>
2022-07-07 09:12:25 +0300

    102. Aggregation Pipeline: Matching and Grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,10 +6,12 @@ const {
   getTour,
   updateTour,
   aliasTopFiveTours,
+  getTourStats,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
+router.route('/tour-stats').get(getTourStats);
 router.route('/top-5-cheap').get(aliasTopFiveTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
