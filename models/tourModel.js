c90bfef0a6c3e40e35ef664084534c8d3b5a9a43
nic3guy <krabik2342@gmail.com>
2022-07-07 12:53:16 +0300

    109. Data Validation: Custom Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const valiator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -42,7 +43,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (inputVal) {
+          // this only points to current doc on NEW DOCUMENT CREATION
+          return inputVal < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
