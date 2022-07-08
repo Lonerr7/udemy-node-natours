6f1767ae99c589414abec8a76a49a0e16e026655
nic3guy <krabik2342@gmail.com>
2022-07-08 09:33:17 +0300

    112. Handling Unhandled Routes

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.use(express.static(`${__dirname}/public`));
 // ========== Mounting Routers ==================
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+// Handling error when no routes matched
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'error',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+  next();
+});
 
 // ========== Server starting ==================
 module.exports = app;
