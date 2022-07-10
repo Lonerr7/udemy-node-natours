99ac39baf9da269640e5d9e52f5fe12963a91b5c
Lonerr7 <krabik2342@gmail.com>
2022-07-10 13:56:16 +0300

    123. Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// Handling Uncaught Exceptions
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION. SHUTTING DOWN THE SERVER!');
+  console.log(`${err.name}:`, err.message);
+
+  // Closing a server and exiting node app. 0 - no errors, 1 - an error occured
+  process.exit(1);
+});
+
 dotenv.config({
   path: './config.env',
 });
@@ -27,8 +36,11 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+// Handling Unhandled Rejections
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION. SHUTTING DOWN THE SERVER!');
+  console.log(`${err.name}:`, err.message);
+  
   // Closing a server and exiting node app. 0 - no errors, 1 - an error occured
   server.close(() => {
     process.exit(1);
