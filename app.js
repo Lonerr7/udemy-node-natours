c52b0bfbbc3c6e64a882af84f3e1980bb4ce2537
nic3guy <krabik2342@gmail.com>
2022-07-14 09:37:10 +0300

    143. Implementing Rate Limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globallErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,18 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// ========== Middlewares ==================
+// ========== Global Middlewares ==================
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many requests from this IP! Please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
